7cb6a5cb28ebf928cfdcad29ce4f52a178023f45
NRHANE <nouraltf37@gmail.com>
2022-04-18 15:56:23 +0100

    ReactComponent

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProfilePhoto from "./Component/Profil/ProfilePhoto"
+import FullName from "./Component/Profil/FullName"
+import Address from "./Component/Profil/Address"
+function App() {
+  return (
+    <div className="appContainer">
+    <ProfilePhoto />
+    <FullName/>
+    <Address />
+    </div>
+    );}
+export default App;
